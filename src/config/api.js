deeb6120ef747cc9dba6c97e2e39ceed08948c95
Dhruv Ozarkar <realdhruvozarkar@gmail.com>
2026-04-05 17:33:04 +0530

    configured for production deployment

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // API Configuration
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  'https://barista-london-task-backend-production.up.railway.app/'
+  'https://barista-london-task-backend-production.up.railway.app'
 
 export const API_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/user/login`,
